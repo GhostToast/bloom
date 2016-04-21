031e50f6a2115e6ea55c978d3d6d1f16b19314ee
Gustave F. Gerhardt <gus@thewirecutter.com>
2016-04-21 16:07:11 -0500

    Sanitize and save fields

--- bloom/taxonomy-bloom-client.php
+++ bloom/taxonomy-bloom-client.php
@@ -50,35 +50,37 @@ function bloom_client_edit_term_meta_fields() {
 
 	$term_id = filter_input( INPUT_GET, 'tag_ID', FILTER_CALLBACK, array( 'options' => 'absint' ) );
 
+	wp_nonce_field( 'custom_bloom-client_meta', '_wpnonce_bloom-client' );
+
 	if ( ! empty( $screen->taxonomy ) && 'bloom-client' === $screen->taxonomy && ! empty( $term_id ) ) :
-		$client_first_name = get_term_meta( $term_id, 'client_first_name', true );
+		$client_first_name  = get_term_meta( $term_id, 'client_first_name', true );
 		$client_middle_name = get_term_meta( $term_id, 'client_middle_name', true );
-		$client_last_name = get_term_meta( $term_id, 'client_last_name', true );
+		$client_last_name   = get_term_meta( $term_id, 'client_last_name', true );
 
 		$client_address_street_1 = get_term_meta( $term_id, 'client_address_street_1', true );
 		$client_address_street_2 = get_term_meta( $term_id, 'client_address_street_2', true );
-		$client_address_city = get_term_meta( $term_id, 'client_address_street_city', true );
-		$client_address_state = get_term_meta( $term_id, 'client_address_street_state', true );
+		$client_address_city     = get_term_meta( $term_id, 'client_address_city', true );
+		$client_address_state    = get_term_meta( $term_id, 'client_address_state', true );
+		$client_address_zip      = get_term_meta( $term_id, 'client_address_zip', true );
 		if ( empty( $client_address_state ) ) {
 			$client_address_state = 'IL';
 		}
-		$client_address_zip = get_term_meta( $term_id, 'client_address_street_zip', true );
 
-		$client_dob = get_term_meta( $term_id, 'client_dob', true );
+		$client_dob     = get_term_meta( $term_id, 'client_dob', true );
 		$client_phone_1 = get_term_meta( $term_id, 'client_phone_1', true );
 		$client_phone_2 = get_term_meta( $term_id, 'client_phone_2', true );
-		$client_email = get_term_meta( $term_id, 'client_email', true );
+		$client_email   = get_term_meta( $term_id, 'client_email', true );
 
-		$emergency_name = get_term_meta( $term_id, 'emergency_name', true );
+		$emergency_name  = get_term_meta( $term_id, 'emergency_name', true );
 		$emergency_phone = get_term_meta( $term_id, 'emergency_phone', true );
 
-		$insurance_provider = get_term_meta( $term_id, 'insurance_provider', true );
-		$insurance_group_number = get_term_meta( $term_id, 'insurance_group_number', true );
-		$insurance_member_id = get_term_meta( $term_id, 'insurance_member_id', true );
-		$insurance_policy_holder_name = get_term_meta( $term_id, 'insurance_policy_holder_name', true );
-		$insurance_policy_holder_dob = get_term_meta( $term_id, 'insurance_policy_holder_dob', true );
+		$insurance_provider               = get_term_meta( $term_id, 'insurance_provider', true );
+		$insurance_group_number           = get_term_meta( $term_id, 'insurance_group_number', true );
+		$insurance_member_id              = get_term_meta( $term_id, 'insurance_member_id', true );
+		$insurance_policy_holder_name     = get_term_meta( $term_id, 'insurance_policy_holder_name', true );
+		$insurance_policy_holder_dob      = get_term_meta( $term_id, 'insurance_policy_holder_dob', true );
 		$insurance_customer_service_phone = get_term_meta( $term_id, 'insurance_customer_service_phone', true );
-		$insurance_benefit_information = get_term_meta( $term_id, 'insurance_benefit_information', true );
+		$insurance_benefit_information    = get_term_meta( $term_id, 'insurance_benefit_information', true );
 		?>
 		<tr><td colspan="2"><div><hr /></div></td></tr>
 
@@ -329,37 +331,43 @@ add_action( 'bloom-client_edit_form_fields', 'bloom_client_edit_term_meta_fields
  * @param string $tt_id   TT id.
  */
 function bloom_client_save_meta( $term_id, $tt_id ) {
-	$nonce = filter_input( INPUT_POST, '_wpnonce_client', FILTER_CALLBACK, array( 'options' => 'sanitize_key' ) );
-	if ( ! wp_verify_nonce( $nonce, 'custom_client_meta' ) ) {
+	$nonce = filter_input( INPUT_POST, '_wpnonce_bloom-client', FILTER_CALLBACK, array( 'options' => 'sanitize_key' ) );
+	if ( ! wp_verify_nonce( $nonce, 'custom_bloom-client_meta' ) ) {
 		return;
 	}
 
-	$client_icon_id = filter_input( INPUT_POST, 'client_icon_id', FILTER_CALLBACK, array( 'options' => 'absint' ) );
-	if ( empty( $client_icon_id ) ) {
-		delete_term_meta( $term_id, 'client_icon_id' );
-	} else {
-		update_term_meta( $term_id, 'client_icon_id', $client_icon_id );
-	}
+	$sanitized_inputs['client_first_name']  = filter_input( INPUT_POST, 'client_first_name', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['client_middle_name'] = filter_input( INPUT_POST, 'client_middle_name', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['client_last_name']   = filter_input( INPUT_POST, 'client_last_name', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
 
-	$client_singular_name = filter_input( INPUT_POST, 'client_singular_name', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
-	if ( empty( $client_singular_name ) ) {
-		delete_term_meta( $term_id, 'client_singular_name' );
-	} else {
-		update_term_meta( $term_id, 'client_singular_name', $client_singular_name );
-	}
+	$sanitized_inputs['client_address_street_1'] = filter_input( INPUT_POST, 'client_address_street_1', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['client_address_street_2'] = filter_input( INPUT_POST, 'client_address_street_2', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['client_address_city']     = filter_input( INPUT_POST, 'client_address_city', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['client_address_state']    = filter_input( INPUT_POST, 'client_address_state', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['client_address_zip']      = filter_input( INPUT_POST, 'client_address_zip', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
 
-	$client_image_id = filter_input( INPUT_POST, 'client_image_id', FILTER_CALLBACK, array( 'options' => 'absint' ) );
-	if ( empty( $client_image_id ) ) {
-		delete_term_meta( $term_id, 'client_image_id' );
-	} else {
-		update_term_meta( $term_id, 'client_image_id', $client_image_id );
-	}
+	$sanitized_inputs['client_dob']     = filter_input( INPUT_POST, 'client_dob', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['client_phone_1'] = filter_input( INPUT_POST, 'client_phone_1', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['client_phone_2'] = filter_input( INPUT_POST, 'client_phone_2', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['client_email']   = filter_input( INPUT_POST, 'client_email', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
 
-	$client_experts = sanitize_text_field( filter_input( INPUT_POST, 'client_experts', FILTER_CALLBACK, array( 'options' => 'wp_unslash' ) ) );
-	if ( empty( $client_experts ) ) {
-		delete_term_meta( $term_id, 'client_experts' );
-	} else {
-		update_term_meta( $term_id, 'client_experts', $client_experts );
+	$sanitized_inputs['emergency_name']  = filter_input( INPUT_POST, 'emergency_name', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['emergency_phone'] = filter_input( INPUT_POST, 'emergency_phone', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+
+	$sanitized_inputs['insurance_provider']               = filter_input( INPUT_POST, 'insurance_provider', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['insurance_group_number']           = filter_input( INPUT_POST, 'insurance_group_number', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['insurance_member_id']              = filter_input( INPUT_POST, 'insurance_member_id', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['insurance_policy_holder_name']     = filter_input( INPUT_POST, 'insurance_policy_holder_name', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['insurance_policy_holder_dob']      = filter_input( INPUT_POST, 'insurance_policy_holder_dob', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['insurance_customer_service_phone'] = filter_input( INPUT_POST, 'insurance_customer_service_phone', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+	$sanitized_inputs['insurance_benefit_information']    = filter_input( INPUT_POST, 'insurance_benefit_information', FILTER_CALLBACK, array( 'options' => 'sanitize_text_field' ) );
+
+	foreach ( $sanitized_inputs as $key => $value ) {
+		if ( empty( $value ) ) {
+			delete_term_meta( $term_id, $key );
+		} else {
+			update_term_meta( $term_id, $key, $value );
+		}
 	}
 }
 add_action( 'created_bloom-client', 'bloom_client_save_meta', 10, 2 );
